1b8464535109650e6cd4d00af572c9eafa73f967
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-12 18:06:32 +0100

    using redux data in react component via useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,21 @@
+import { useSelector } from 'react-redux';
+// Default Redux hook allowing us select a part of our state menaged by the store.
+// for classbassed component we have to use 'connect' hook from redux.
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	const counter = useSelector((state) => state.counter);
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const toggleCounterHandler = () => {};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
