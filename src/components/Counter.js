4bd0526adf67fc122b14aecdcacb2dbcf0ac4939
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-13 17:54:43 +0100

    added an dispatch action that works with multiple states properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
 	const dispatch = useDispatch();
 	const counter = useSelector((state) => state.counter);
+	const show = useSelector((state) => state.showCounter);
 
 	const incrementHandler = () => {
 		dispatch({ type: 'increment' });
@@ -14,16 +15,19 @@ const Counter = () => {
 	const decrementHandler = () => {
 		dispatch({ type: 'decrement' });
 	};
+
 	const increaseHandler = () => {
 		dispatch({ type: 'increase', amount: 10 });
 	};
 
-	const toggleCounterHandler = () => {};
+	const toggleCounterHandler = () => {
+		dispatch({ type: 'toggle' });
+	};
 
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>{counter}</div>
+			{show && <div className={classes.value}>{counter}</div>}
 			<div>
 				<button onClick={incrementHandler}>Increment</button>
 				<button onClick={increaseHandler}>Increse by 10</button>
