af6fdf3015c72ff84a4fcc092c1c58735c1034cf
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-13 20:25:58 +0100

    added reading & dispatching from a new slice to a buttons to log in and log of

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Counter from './components/Counter';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  return (
-    <Counter />
-  );
+	const isLogin = useSelector((state) => state.auth.isAuthentiated);
+	return (
+		<Fragment>
+			<Header />
+			{!isLogin && <Auth />}
+			{isLogin && <UserProfile />}
+			<Counter />
+		</Fragment>
+	);
 }
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
-
-// This same as react redux but using reduxjs/toolkit
-// here we can muttate code when we use reduxjs/toolkit
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
 	name: 'counter',
-	initialState, // = initialState: initialState
+	initialState: initialCounterState,
 	reducers: {
 		increment(state) {
 			state.counter++;
@@ -16,7 +13,7 @@ const counterSlice = createSlice({
 			state.counter--;
 		},
 		increase(state, action) {
-			state.counter = state.counter + action.payload; // here additional paramenters store in action.payload
+			state.counter = state.counter + action.payload;
 		},
 		toggleCounter(state) {
 			state.showCounter = !state.showCounter;
@@ -24,10 +21,27 @@ const counterSlice = createSlice({
 	},
 });
 
+const initialAuthState = {
+	isAuthentiated: false,
+};
+
+const authSlice = createSlice({
+	name: 'authentication',
+	initialState: initialAuthState,
+	reducers: {
+		login(state) {
+			state.isAuthentiated = true;
+		},
+		logout(state) {
+			state.isAuthentiated = false;
+		},
+	},
+});
+
 const store = configureStore({
-	reducer: counterSlice.reducer,
+	reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
-
+export const authActions = authSlice.actions;
 export default store;
